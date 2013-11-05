703dffcc5d57b7b86ff77c54fa264d8a0252e84c
Yifeng Jiang <uprushworld@gmail.com>
2013-11-05 17:00:16 +0900

    include ark::default

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,6 +17,8 @@
 # limitations under the License.
 #
 
+include_recipe "ark"
+
 unless Chef::Config[:solo]
   es_server_results = search(:node, "roles:#{node['kibana']['es_role']} AND chef_environment:#{node.chef_environment}")
   unless es_server_results.empty?
